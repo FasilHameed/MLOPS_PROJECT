22eda401be875c2ae3232d653bc2f3c824ac7216
Fasil hameed <faisalhameed763@gmail.com>
2024-01-20 22:37:41 +0530

    Restructured the folder

--- template.py
+++ template.py
@@ -31,7 +31,7 @@ list_of_files = [
     "tox.ini",
     "experiment/experiments.ipynb",
     "src/logger/logging.py",
-    "src/exception/exception" 
+    "src/exception/exception.py" 
 ]
 
 # Loop through the list of files
